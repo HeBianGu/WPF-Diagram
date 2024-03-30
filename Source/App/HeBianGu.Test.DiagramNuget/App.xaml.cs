58e7df9252cce44d59169c9d2c7fc29acfe5fb1e
HeBianGu <908293466@qq.com>
2024-03-30 17:54:37 +0800

    +

---
+++ Source/App/HeBianGu.Test.DiagramNuget/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace HeBianGu.Test.DiagramNuget
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
